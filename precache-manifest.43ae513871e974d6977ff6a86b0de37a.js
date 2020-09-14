3ddfa54f37d676e480e348d3abe2aae3828ad25b
Philip Cook <voodoo.science.programming@gmail.com>
2020-09-14 12:37:59 +0100

    Updates

--- precache-manifest.43ae513871e974d6977ff6a86b0de37a.js
+++ precache-manifest.43ae513871e974d6977ff6a86b0de37a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b8824e5dfde4875f57ed5f073772d817",
+    "revision": "b575245d4712205b8f50eb365896ed7b",
     "url": "/robofriends/index.html"
   },
   {
-    "revision": "939c29a12718c22b5acf",
+    "revision": "abcf402c9619777cd68e",
     "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "b9802bade02678129dd5",
+    "revision": "41bd2786551e6a460185",
     "url": "/robofriends/static/css/main.4ee257b7.chunk.css"
   },
   {
-    "revision": "939c29a12718c22b5acf",
-    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b9802bade02678129dd5",
-    "url": "/robofriends/static/js/main.73b06cc2.chunk.js"
+    "revision": "41bd2786551e6a460185",
+    "url": "/robofriends/static/js/main.a1f71467.chunk.js"
   },
   {
     "revision": "e7d2cbb368e0813da61d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.2e678a547770914eda97d94ac0f064d9.js"
+  "/robofriends/precache-manifest.43ae513871e974d6977ff6a86b0de37a.js"
 );
 
 self.addEventListener('message', (event) => {
